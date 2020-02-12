9c9df09e31e20c8a3a2df31c3a31a6f563e67d8d
Alexander EG <al3xito2515@gmail.com>
2020-02-12 11:46:01 -0500

    fx show posts

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 // Route::get('nosotros', 'PagesController@about')->name('pages.about');
 // Route::get('archivo', 'PagesController@archive')->name('pages.archive');
 // Route::get('contacto', 'PagesController@contact')->name('pages.contact');
-Route::get('blog/{post}', 'PostsController@show')->name('posts.show');
+// Route::get('blog/{post}', 'PostsController@show')->name('posts.show');
 // Route::get('categorias/{category}', 'CategoriesController@show')->name('categories.show');
 // Route::get('tags/{tag}', 'TagsController@show')->name('tags.show');
 
